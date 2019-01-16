cbea0589d1ba02c5a97ab6fa1e2ea7c647024186
kadimka <dima.litvinenko.dev@gmail.com>
2019-01-16 12:57:12 +0200

    Upload image logic commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^resume$', views.resume),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mycvblog/settings.py
+++ mycvblog/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 
 STATIC_URL = '/blog/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'blog/media')
