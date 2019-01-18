0742823fc8e0b4e3b1f7d8e5bc7e683b68039abc
kadimka <dima.litvinenko.dev@gmail.com>
2019-01-18 13:00:26 +0200

    add post fix

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 from .forms import PostForm
+from django.http import HttpResponse, HttpResponseRedirect
 from  django.shortcuts import redirect
 
 # Create your views here.
@@ -28,9 +29,9 @@ def post_new(request):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-        else:
-            form = PostForm()
-            return render(request, 'blog/post_edit.html', {'form':form})
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form':form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
