e10dcb1e88474e8b1de4ce99c4c915deb69ac4dc
kadimka <dima.litvinenko.dev@gmail.com>
2019-01-18 12:36:36 +0200

    add editing post

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^resume$', views.resume),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
