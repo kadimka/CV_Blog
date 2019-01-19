7a1d1fd67234342e41183638e515e20516587ee1
kadimka <dima.litvinenko.dev@gmail.com>
2019-01-19 19:49:05 +0200

    commit login/logout logic

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth import views as dviews
+
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_list, name='post_list'),
     url(r'^resume$', views.resume),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
@@ -13,4 +15,6 @@ urlpatterns = [
     url('drafts/', views.post_draft_list, name='post_draft_list'),
     url('post/(?P<pk>\d+)/publish/', views.post_publish, name='post_publish'),
     url('post/(?P<pk>\d+)/remove/', views.post_remove, name='post_remove'),
+    url('accounts/login/', dviews.LoginView.as_view(), name='login'),
+    url('accounts/logout/', dviews.LogoutView.as_view(next_page='/'), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mycvblog/settings.py
+++ mycvblog/settings.py
@@ -122,3 +122,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'blog/media')
+
+LOGIN_REDIRECT_URL = '/'
